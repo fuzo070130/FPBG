404ccdd444f668db62c44a464f64b3e9872a7975
km <km@km-PC>
2017-11-16 14:54:10 +0900

    게시판

--- FPBG/src/main/java/com/FPBG/www/BoardController.java
+++ FPBG/src/main/java/com/FPBG/www/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.FPBG.domain.vo.BoardVO;
 import com.FPBG.service.BoardService;
@@ -27,5 +28,21 @@ public class BoardController {
 	public void registerGET(BoardVO board, Model model) throws Exception {
 
 		logger.info("register get ...........");
+	}
+	
+	@RequestMapping(value = "/register", method =  RequestMethod.POST)
+	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
+		System.out.println(board.getBoardContent());
+		
+		service.create(board);
+		
+		rttr.addFlashAttribute("msg", "success");
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	  public void listAll(Model model) throws Exception {
+		
+		model.addAttribute("list", service.listAll());
 	}
 }
