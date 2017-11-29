2056d9a93626da0baf188cd961e569c36d31e118
km <km@km-PC>
2017-11-29 18:06:59 +0900

    123

--- FPBG/src/main/java/com/FPBG/www/SearchBoardController.java
+++ FPBG/src/main/java/com/FPBG/www/SearchBoardController.java
@@ -1,6 +1,10 @@
 package com.FPBG.www;
 
+import java.io.File;
+import java.util.UUID;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.FPBG.domain.dto.PageMaker;
 import com.FPBG.domain.vo.BoardVO;
+import com.FPBG.domain.vo.PhotoVO;
 import com.FPBG.domain.vo.SearchCriteria;
 import com.FPBG.service.BoardService;
 
@@ -92,7 +97,6 @@ public class SearchBoardController {
   @RequestMapping(value = "/register", method = RequestMethod.POST)
   public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
 
-<<<<<<< HEAD
     logger.info("regist post ...........");
     logger.info(board.toString());
     
@@ -100,13 +104,44 @@ public class SearchBoardController {
     String contentMain = content.substring(3 , content.length()-4);
     board.setBoardContent(contentMain);*/
     
-=======
->>>>>>> branch 'master' of https://github.com/fuzo070130/FPBG.git
     service.create(board);
 
     rttr.addFlashAttribute("msg", "SUCCESS");
 
     return "redirect:/sboard/list";
   }
-
- }
+  
+  @RequestMapping("/photoUpload")
+  public String photoUpload(HttpServletRequest request, PhotoVO vo){
+      String callback = vo.getCallback();
+      String callback_func = vo.getCallback_func();
+      String file_result = "";
+      try {
+          if(vo.getFiledata() != null && vo.getFiledata().getOriginalFilename() != null && !vo.getFiledata().getOriginalFilename().equals("")){
+              //파일이 존재하면
+              String original_name = vo.getFiledata().getOriginalFilename();
+              String ext = original_name.substring(original_name.lastIndexOf(".")+1);
+              //파일 기본경로
+              String defaultPath = request.getSession().getServletContext().getRealPath("/");
+              //파일 기본경로 _ 상세경로
+              String path = defaultPath + "resources" + File.separator + "photo_upload" + File.separator;             
+              File file = new File(path);
+              System.out.println("path:"+path);
+              //디렉토리 존재하지 않을경우 디렉토리 생성
+              if(!file.exists()) {
+                  file.mkdirs();
+              }
+              //서버에 업로드 할 파일명(한글문제로 인해 원본파일은 올리지 않는것이 좋음)
+              String realname = UUID.randomUUID().toString() + "." + ext;
+          ///////////////// 서버에 파일쓰기 /////////////////
+              vo.getFiledata().transferTo(new File(path+realname));
+              file_result += "&bNewLine=true&sFileName="+original_name+"&sFileURL=/FPBG/resources/photo_upload/"+realname;
+          } else {
+              file_result += "&errstr=error";
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return "redirect:" + callback + "?callback_func="+callback_func+file_result;
+  }
+}
